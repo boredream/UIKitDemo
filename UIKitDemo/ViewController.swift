3b8aec44c78c9afe5b7a5d3e2c1f07407ffd4c07
boredream <48262906@qq.com>
2022-08-18 16:58:14 +0800

    Initial commit

---
+++ UIKitDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  UIKitDemo
+//
+//  Created by lcy on 2022/8/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("a ghost !")
+    }
+}
+
