ce51d134a0dfb91a5d2068a9c09a9d4643afe249
boredream <48262906@qq.com>
2022-08-19 12:59:32 +0800

    game rule

--- UIKitDemo/Concentration.swift
+++ UIKitDemo/Concentration.swift
@@ -20,8 +20,31 @@ class Concentration {
     
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        cards[index].isFaceUp.toggle()
+        if !cards[index].isMatched {
+            // 如果选中卡片是未匹配的
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // 如果有已经翻起的卡片，且当前选择的不是这张卡
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    // 翻起的卡正好是匹配的
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // 其它情况
+                for flipDownIndex in cards.indices {
+                    // 先所有卡朝下
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                // 目标卡片朝上
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
     
 }
